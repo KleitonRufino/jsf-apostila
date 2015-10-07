0d491a65020ff6642c511171b0ae2f9188967996
Kleiton Rufino <=>
2015-10-07 17:28:04 -0300

    templates-modularizacao

---
+++ templates-modularizacao/src/br/com/controlador/InstrutorBean.java
@@ -0,0 +1,34 @@
+package br.com.controlador;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+
+import br.com.modelo.Instrutor;
+
+@ManagedBean
+public class InstrutorBean {
+
+	private List<Instrutor> instrutores = new ArrayList<Instrutor>();
+
+	public InstrutorBean() {
+		Instrutor rafael = new Instrutor();
+		rafael.setNome("Rafael Cosentino");
+		rafael.setDataDeNascimento("30/10/1984");
+		Instrutor marcelo = new Instrutor();
+		marcelo.setNome("Marcelo Martins");
+		marcelo.setDataDeNascimento("02/04/1985");
+		this.instrutores.add(rafael);
+		this.instrutores.add(marcelo);
+	}
+
+	public List<Instrutor> getInstrutores() {
+		return instrutores;
+	}
+
+	public void setInstrutores(List<Instrutor> instrutores) {
+		this.instrutores = instrutores;
+	}
+
+}
