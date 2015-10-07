19cebad5649cce9299c2fa0cda4e7e5c2b7da274
Kleiton Rufino <=>
2015-10-07 17:25:03 -0300

    eventos

---
+++ eventos/src/br/com/controle/CadastroBean.java
@@ -0,0 +1,99 @@
+package br.com.controle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ValueChangeEvent;
+
+import br.com.model.Estado;
+
+@ManagedBean
+@SessionScoped
+public class CadastroBean {
+
+	private String nome;
+	private String cidade;
+	private String siglaDoEstadoEscolhido;
+
+	private Estado estadoSelecionado = new Estado();
+	private List<Estado> estados = new ArrayList<>();
+
+	public CadastroBean() {
+		Estado sp = new Estado();
+		sp.setSigla("SP");
+		sp.setNome("São Paulo");
+		sp.getCidades().add("São Paulo");
+		sp.getCidades().add("Campinas");
+
+		Estado rj = new Estado();
+		rj.setSigla("RJ");
+		rj.setNome("Rio de Janeiro");
+		rj.getCidades().add("Rio de Janeiro");
+		rj.getCidades().add("Niterói");
+
+		Estado rn = new Estado();
+		rn.setSigla("RN");
+		rn.setNome("Rio Grande do Norte");
+		rn.getCidades().add("Natal");
+		rn.getCidades().add("Mossoró");
+
+		this.estados.add(sp);
+		this.estados.add(rj);
+		this.estados.add(rn);
+	}
+
+	public void mudaEstado(ValueChangeEvent event) {
+		this.siglaDoEstadoEscolhido = event.getNewValue().toString();
+		for (Estado estado : estados) {
+			if (estado.getSigla().equals(this.siglaDoEstadoEscolhido)) {
+				this.estadoSelecionado = estado;
+				break;
+			}
+		}
+		FacesContext.getCurrentInstance().renderResponse();
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getSiglaDoEstadoEscolhido() {
+		return siglaDoEstadoEscolhido;
+	}
+
+	public void setSiglaDoEstadoEscolhido(String siglaDoEstadoEscolhido) {
+		this.siglaDoEstadoEscolhido = siglaDoEstadoEscolhido;
+	}
+
+	public Estado getEstadoSelecionado() {
+		return estadoSelecionado;
+	}
+
+	public void setEstadoSelecionado(Estado estadoSelecionado) {
+		this.estadoSelecionado = estadoSelecionado;
+	}
+
+	public List<Estado> getEstados() {
+		return estados;
+	}
+
+	public void setEstados(List<Estado> estados) {
+		this.estados = estados;
+	}
+
+}
